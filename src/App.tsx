ea803db54538743cba2cd853cfa432dbe92f2543
Matt Nichols <mattnichols206@gmail.com>
2025-07-01 13:32:16 -0700

    feat: add test svg and background param

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import CanvasStage from './components/CanvasStage'
 import './App.css'
+import defaultBgUrl from './assets/wildflowers.png'
+
+function useBackgroundUrl(): string {
+  const params = new URLSearchParams(window.location.search)
+  return params.get('bg') || defaultBgUrl
+}
 
 export default function App() {
+  const bgUrl = useBackgroundUrl()
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div
+      className="w-screen h-screen overflow-hidden bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgUrl})` }}
+    >
       <CanvasStage />
     </div>
   )
