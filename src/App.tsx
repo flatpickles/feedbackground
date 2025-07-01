dbe891f24750fbd999f5fb2bff440c164e692f3c
Matt Nichols <mattnichols206@gmail.com>
2025-07-01 12:38:00 -0700

    Initialize Vite React project with R3F and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import CanvasStage from './components/CanvasStage'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <CanvasStage />
+    </div>
+  )
+}
---
+++ src/components/CanvasStage.tsx
@@ -0,0 +1,9 @@
+import { Canvas } from '@react-three/fiber'
+
+export default function CanvasStage() {
+  return (
+    <Canvas className="w-full h-full">
+      {/* TODO: Add scene components in later milestones */}
+    </Canvas>
+  )
+}
