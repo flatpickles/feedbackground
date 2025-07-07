66c330d3c263e055f00b01ef82aea640c1a59b2f
Matt Nichols <mattnichols206@gmail.com>
2025-07-06 18:27:38 -0700

    Fix tailwind; overview panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import CanvasStage from './components/CanvasStage'
+import { useState } from 'react'
 import './App.css'
 import wildflowersUrl from './assets/wildflowers.png'
-import { useState } from 'react'
+import CanvasStage from './components/CanvasStage'
+import Overview from './components/Overview'
 
 function useInitialBgName(): 'wildflowers' | 'white' {
   const params = new URLSearchParams(window.location.search)
@@ -15,11 +16,14 @@ export default function App() {
       ? { backgroundImage: `url(${wildflowersUrl})` }
       : { backgroundColor: '#ffffff' }
   return (
-    <div
-      className="w-screen h-screen overflow-hidden bg-cover bg-center"
-      style={style}
-    >
-      <CanvasStage backgroundName={bgName} setBackgroundName={setBgName} />
-    </div>
+    <>
+      <Overview />
+      <div
+        className="w-screen h-screen overflow-hidden bg-cover bg-center"
+        style={style}
+      >
+        <CanvasStage backgroundName={bgName} setBackgroundName={setBgName} />
+      </div>
+    </>
   )
 }
---
+++ src/components/Overview.tsx
@@ -0,0 +1,19 @@
+export default function Overview() {
+  return (
+    <div className="font-mono flex flex-col gap-2 fixed top-0 left-0 sm:w-[400px] sm:right-auto right-0 z-10 bg-white p-4 m-2 rounded-lg">
+      <h1 className="text-lg font-bold">feedbackground</h1>
+      <p className="text-xs">
+        Feedback in the background. Grab the foreground layer below and move it
+        around to see the effect. You can adjust the params to your heart’s
+        content on a big screen. See the implementation{' '}
+        <a
+          href="https://github.com/flatpickles/feedbackground"
+          className="underline"
+        >
+          here
+        </a>
+        .
+      </p>
+    </div>
+  )
+}
